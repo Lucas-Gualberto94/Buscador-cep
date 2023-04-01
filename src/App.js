7bcb477ebc40f976123ddd4ce42df43b78f6a8a4
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2023-04-01 15:20:47 +0100

    start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import{ FiSearch } from 'react-icons/fi'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <h1 className="title">Buscador CEP</h1>
+      <div className="containerInput">
+        <input 
+        type="text"
+        placeholder="Digite seu cep..."
+        />
+
+        <button className="buttonSerach">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 79003222</h2>
+        
+        <span>Rua teste algum</span>
+        <span>Complemento: algum complemento</span>
+        <span>Vila Rosa</span>
+        <span>Campo Grande- SP</span>
+
+      </main>
+
     </div>
   );
 }
